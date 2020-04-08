246cdd0fda42bf6a4efd49f1599183c6ecd42954
WandersonMJ <wanderson.marques@fatecitapetininga.edu.br>
2020-04-08 11:00:54 -0300

    challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,19 +11,50 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json({ message: "sucess", repositories})
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  repository = {
+      id: uuid(), 
+      title,
+      url,
+      techs,
+      likes: 0 };
+    repositories.push(repository);
+  return response.json({ message: 'sucess', repository})
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  
+  if (repositoryIndex < 0){
+    return response.status(400).json({ error: "respository not found"})
+  }
+
+  const { likes } = repositories[repositoryIndex];
+
+  repositories[repositoryIndex] = {id, title, url, techs, likes};
+ 
+  return response.json({message: "sucess", repository: repositories[repositoryIndex]});
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  
+  if (repositoryIndex < 0){
+    return res.status(400).json({ error: "respository alredy been removed, or doesn't exists."})
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).send([])
 });
 
 app.post("/repositories/:id/like", (request, response) => {
